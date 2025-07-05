77e33938578dbab76da0dffe5bfbad6cad6fd2f4
Gaétan Collaud <gaetancollaud@gmail.com>
2025-07-05 14:36:05 +0200

    try github action

--- src/main/webui/src/app/app.ts
+++ src/main/webui/src/app/app.ts
@@ -1,14 +1,13 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MqttStoreService} from './services/mqtt-store.service';
 import {DisplayTree} from './components/display-tree/display-tree';
 import {TreeItem} from './types/tree-item';
 import {DisplayTopicMessages} from './components/display-topic-messages/display-topic-messages';
-import {JsonPipe} from '@angular/common';
 import {MatToolbar} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [DisplayTree, DisplayTopicMessages, JsonPipe, MatToolbar],
+  imports: [DisplayTree, DisplayTopicMessages, MatToolbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/test/java/net/collaud/gaetan/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package net.collaud.gaetan;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
