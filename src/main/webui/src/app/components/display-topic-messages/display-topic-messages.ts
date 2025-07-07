40ea05846f408a6e73ae16329d07104e4e8a2a38
Gaétan Collaud <gaetancollaud@gmail.com>
2025-07-07 11:18:39 +0200

    fix history sizing

--- src/main/webui/src/app/components/display-topic-messages/display-topic-messages.ts
+++ src/main/webui/src/app/components/display-topic-messages/display-topic-messages.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component, input, signal} from '@angular/core';
 import {TreeItem} from '../../types/tree-item';
 import {MqttMessage} from '../../generated/openapi';
 import {MatCardModule} from '@angular/material/card';
@@ -11,14 +11,16 @@ import {
   MatExpansionPanelHeader,
   MatExpansionPanelTitle
 } from '@angular/material/expansion';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-display-topic-messages',
-  imports: [MatCardModule, MqttMessageContent, TruncatedTextWithCopy, MatAccordion, MatExpansionPanel, MatExpansionPanelTitle, MatExpansionPanelHeader, MatExpansionPanelDescription],
+  imports: [MatCardModule, MqttMessageContent, TruncatedTextWithCopy, MatAccordion, MatExpansionPanel, MatExpansionPanelTitle, MatExpansionPanelHeader, MatExpansionPanelDescription, MatButton],
   templateUrl: './display-topic-messages.html',
   styleUrl: './display-topic-messages.scss'
 })
 export class DisplayTopicMessages {
   node = input.required<TreeItem | undefined>();
   messages = input.required<MqttMessage[]>();
+  showHistory = signal<boolean>(false);
 }
